876b9adb52f022cc4c176cc79894f20dfbc0d22a
Billy Mathison <wlmathison@gmail.com>
2019-03-20 10:07:00 -0500

    completing map function, filter function, and find function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,6 +16,9 @@ const outEl = document.querySelector("#output")
 //     outEl.innerHTML += "<hr/>"
 // });
 
+
+// ************************************************************************
+
 // Lightning Exercise: Add another section sibling to the current one and use object dot notation to display each company's city. Use square bracket notation to display the state code. Use dynamic square bracket notation to add the zip code.
 
 // Array to contain all the New York businesses
@@ -41,24 +44,171 @@ const outEl = document.querySelector("#output")
 //     outEl.innerHTML += "<hr/>"
 // });
 
-const manufacturingBusinesses = businesses.filter(business => {
-    let manufacturing = false
+/***********************************************************************/
 
-    if (business.companyIndustry === "Manufacturing") {
-        manufacturing = true
-    }
+// const manufacturingBusinesses = businesses.filter(business => {
+//     let manufacturing = false
 
-    return manufacturing
-})
+//     if (business.companyIndustry === "Manufacturing") {
+//         manufacturing = true
+//     }
 
-outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
+//     return manufacturing
+// })
 
-manufacturingBusinesses.forEach(business => {
-    outEl.innerHTML += `
-      <h2>${business.companyName}</h2>
-      <section>
-        ${business.addressFullStreet}
-      </section>
-    `
-    outEl.innerHTML += "<hr/>"
-});
+// outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
+
+// manufacturingBusinesses.forEach(business => {
+//     outEl.innerHTML += `
+//       <h2>${business.companyName}</h2>
+//       <section>
+//         ${business.addressFullStreet}
+//       </section>
+//     `
+//     outEl.innerHTML += "<hr/>"
+// });
+
+// **********************************************************************
+
+// outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+// const agents = businesses.map(business => {
+//     return business.purchasingAgent
+// })
+
+// console.table(agents)
+
+// agents.forEach(agent => {
+//   outEl.innerHTML += `<h2>${agent.nameFirst} ${agent.nameLast}</h2>`;
+//   outEl.innerHTML += "<hr/>";
+// });
+
+// *******************************************************************
+
+// Lightning Exercise: Instead of just returning the purchasing agent object, return a new object that has the full name of the purchasing agent, the company name, and the phone number. The data structure is shown below. Use that new data structure to display the agent with their company and phone number
+
+// {
+//     "fullName": "Kaylee Gutkowski",
+//     "company": "Highnix",
+//     "phoneNumber": "235.266.6278"
+// }
+
+// My code:
+// const agents = businesses.map(business => {
+//     let agent = {};
+//     agent.fullName = business.purchasingAgent.nameFirst + " " +business.purchasingAgent.nameLast;
+//     agent.company = business.companyName;
+//     agent.phoneNumber = business.phoneWork;
+//     return agent;
+// })
+
+// Better code:
+// const agents = businesses.map(business => {
+//     return {
+//         fullName: `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+//         company: business.companyName,
+//         phoneNumber: business.phoneWork
+//     }
+
+// })
+
+// console.log(agents)
+
+// My code :
+// agents.forEach(agent => {
+//     outEl.innerHTML += `
+//   <h2>${agent.fullName}</h2>
+//   <h3>${agent.company}</h3>
+//   <h3>${agent.phoneNumber}</h3>
+//   `;
+//     outEl.innerHTML += "<hr/>";
+// });
+
+// Jisie's code
+// agents.forEach(agent => {
+//     outEl.innerHTML += `<h2>${agent.fullName}</h2>`;
+//     outEl.innerHTML += `<p>${agent.company}</p>`;
+//     outEl.innerHTML += `<p>${agent.phoneNumber}</p>`;
+//     outEl.innerHTML += "<hr/>";
+// })
+
+// ************************************************************************
+
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business =>
+//                     business.companyName.includes(keyPressEvent.target.value)
+//             );
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.companyName}
+//                 </h2>
+//                 <section>
+//                 ${foundBusiness.addressFullStreet}
+
+//                 </section>
+//                 <section>
+//                 ${foundBusiness.addressCity},
+//                 ${foundBusiness.addressStateCode}
+//                 ${foundBusiness.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+
+// ******************************************************************************
+
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundAgent = businesses.find(
+//                 business =>
+//                     business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value)
+//             );
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundAgent.purchasingAgent.nameFirst} ${foundAgent.purchasingAgent.nameLast}
+//                 </h2>
+//                 <section>
+//                 <p>${foundAgent.companyName}</p>
+//                 <p>${foundAgent.phoneWork}</p>
+//                 </section>
+//             `;
+//         }
+//     });
+
+// ******************************************************************
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+            const foundAgent = businesses.find(
+                business =>
+                business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) || business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
+            );
+
+            outEl.innerHTML = `
+                <h2>
+                ${foundAgent.purchasingAgent.nameFirst} ${foundAgent.purchasingAgent.nameLast}
+                </h2>
+                <section>
+                <p>${foundAgent.companyName}</p>
+                <p>${foundAgent.phoneWork}</p>
+                </section>
+            `;
+        }
+    });
